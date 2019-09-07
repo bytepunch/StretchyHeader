cff3dafb3bc31480d87ea7654bd7cd732b8a69e9
Jörg Klausewitz <joerg@bytepunch.com>
2019-09-07 16:39:48 +0200

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StretchyHeader
+//
+//  Created by Jörg Klausewitz on 07.09.19.
+//  Copyright © 2019 Jörg Klausewitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
